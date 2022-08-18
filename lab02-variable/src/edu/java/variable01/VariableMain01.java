a743403dbf366738413b96adca81abae7f1507e8
ByeonJuHwan <asdcz@navercom>
2022-08-18 13:07:56 +0900

    국비 2일차 오전

--- lab02-variable/src/edu/java/variable01/VariableMain01.java
+++ lab02-variable/src/edu/java/variable01/VariableMain01.java
@@ -20,10 +20,42 @@ public class VariableMain01 {
 		System.out.println(salary);
 		
 		// 자바의 기본 데이터 타입
-		// 정수 타입 : byte, short, int ,long, char
+		// 정수 타입 : byte, short, int, long, char
 		// 실수 타입 : float, double
 		// 논리 타입 : boolean
 		
+		// 변수 이름을 만드는 관습/문법 :
+		// 1. 변수이름은 알파벳과 숫자 , _ 를 사용할 수 있음.
+		// 2. 변수이름은 숫자로 시작할 수 없음
+		// 3. 변수 이름은 소문자로 시작하는 것이 좋음.
+		// 4. 변수 이름이 2개 이상의 단어로 이루어질 때는 camel 표기법을 사용하는 것을 권장. ex) int phoneNumber
+		// 5. 같은 이름으로 변수를 2번 이상 선언할 수 없음.
+		
+		// 정수 2개의 사칙연산 결과를 출력
+		int x = 123;
+		int y = 10;
+		int result;
+		
+		result = x + y;
+		System.out.println(result);
+		
+		result = x - y;
+		System.out.println(result);
+		
+		result = x * y;
+		System.out.println(result);
+		
+		result = x / y;  // (정수) / (정수) 를 나누면 나눈 몫만 결고 값으로 출력 
+		System.out.println(result);	
+		
+		result = x % y; 
+		System.out.println(result);
+		
+		// 나누기 연산자(/)가 실수엣 사용되면 실수 계산을 사용
+		double z = 123.0;
+		double div = z / y;
+		System.out.println(div);
+				
 	}
 
 }
---
+++ lab02-variable/src/edu/java/variable04/VarialbeMain04.java
@@ -0,0 +1,41 @@
+package edu.java.variable04;
+
+import java.util.Scanner;
+
+public class VarialbeMain04 {
+
+	public static void main(String[] args) {
+		// 콘솔 창에서 키보드로 숫자를 입력받아서 변수에 저장
+		// 1. Scanner 타입의 변수를 선언하고 초기화.
+		Scanner sc = new Scanner (System.in);
+		
+		//System.out.println("정수를 입력 >>>>>>");
+		
+		// 2. Scanner 객체를 사용해서 콘솔 창에서 정수를 입력받고 저장.
+		int number  = sc.nextInt();
+		System.out.println("number = " + number);
+		
+		//과제 1)
+		// Java 시험 점수(정수)를 입력
+		// SQL 시험 점수를 입력
+		// JSP 시험 점수를 입력
+		// 세 과목의 총점(정수)을 계산 출력
+		// 세 과목의 평균(실수)을 계산하고 출력
+		
+		System.out.println("JAVA 점수를 입력하세요 >>");
+		int a = sc.nextInt();
+		
+		System.out.println("SQL 점수를 입력하세요>>>");
+		int b = sc.nextInt();
+		
+		System.out.println("JSP 점수를 입력하시요>>>");
+		int c = sc.nextInt();
+		
+		int sum = a + b +c;
+		System.out.println("점수의 총점은 = " + sum);
+		
+		double avg = sum/3;
+		System.out.println("점수의 평균은 = " + avg);
+	}
+
+}
