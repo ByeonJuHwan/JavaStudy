d663bd95a755aad40fd46d5d4529770186ccff63
ByeonJuHwan <asdcz@naver.com>
2022-08-25 18:21:45 +0900

    7일차 과제 분산,표준편차

--- lab05-method/src/edu/java/method05/MethodMain05.java
+++ lab05-method/src/edu/java/method05/MethodMain05.java
@@ -37,6 +37,26 @@ public class MethodMain05 {
         return min;
     }
     
+    public static double bs (int [] scores) {
+        double avg = mean(scores);
+        double bbs;  //(값 -평균)
+        double bbbs; //(값 -평균) 2제곱
+        double sum = 0;
+        for(int i=0; i<scores.length; i++) {
+            bbs=(scores[i]-avg);    //(값 -평균)
+            bbbs = Math.pow(bbs,2); //(값 -평균) 2제곱
+            sum+=bbbs;              //(값 -평균) 2제곱 의 합
+        }
+        double bs = (double)sum/scores.length;  // 거듭제곱 / 배열의 길이
+        return bs;
+    }
+    
+    public static double ps (double bs) {
+        double ps = Math.sqrt(bs);   // 분산의 제곱근
+        return ps;
+    }
+    
+    
     
     
     public static void main(String[] args) {
@@ -55,6 +75,13 @@ public class MethodMain05 {
         
         int min = min(scores);
         System.out.println("최솟값 = " + min); //50
+        
+        double bs = bs(scores);
+        System.out.println("분산 = " + bs);
+        
+        double ps = ps(bs);
+        System.out.println("표준편차 = " + ps);
+        
     }
 
 }
