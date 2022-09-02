6290c18ea0e5550fea62071b33a518d0226d3712
ByeonJuHwan <asdcz@naver.com>
2022-09-02 17:44:21 +0900

    5강완

--- GUIpractice/src/gui/practice/Lesson3.java
+++ GUIpractice/src/gui/practice/Lesson3.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Lesson3 extends JFrame {
 
     private JPanel contentPane;
+    private JTextField textField;
 
     /**
      * Launch the application.
@@ -35,8 +41,25 @@ public class Lesson3 extends JFrame {
         setBounds(100, 100, 687, 451);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JButton btnNewButton = new JButton("New button");
+        btnNewButton.setFont(new Font("굴림", Font.BOLD, 14));
+        btnNewButton.setBounds(499, 290, 138, 79);
+        contentPane.add(btnNewButton);
+        
+        JLabel lblNewLabel = new JLabel("Login Form");
+        lblNewLabel.setBounds(215, 60, 229, 35);
+        contentPane.add(lblNewLabel);
+        
+        textField = new JTextField();
+        textField.setBounds(208, 128, 149, 35);
+        contentPane.add(textField);
+        textField.setColumns(10);
+        
+        JLabel lblNewLabel_1 = new JLabel("Name");
+        lblNewLabel_1.setBounds(85, 138, 82, 25);
+        contentPane.add(lblNewLabel_1);
     }
-
 }
