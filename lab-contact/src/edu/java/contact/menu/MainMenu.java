d8342a85d7b061ea2521b0c67f3ad91918004f5c
ByeonJuHwan <asdcz@naver.com>
2022-09-16 16:05:58 +0900

    19일차 contact ver3

--- lab-contact/src/edu/java/contact/menu/MainMenu.java
+++ lab-contact/src/edu/java/contact/menu/MainMenu.java
@@ -4,7 +4,7 @@ public enum MainMenu {
     // enum : 열거형 상수들을 정의하는 특별한 종류의 "클래스".
     // -> 생성자, 메서드를 정의할 수 있음.
     // -> 변수 타입, 메서드 리턴 타입, 파리미터 타입을 선언할 때 enum 이름을 사용할 수 있음.
-    QUIT, SELECT_ALL, SELECT_BY_INDEX, CREATE, UPDATE, UNKNOWN;
+    QUIT, SELECT_ALL, SELECT_BY_INDEX, CREATE, UPDATE, DELETE, UNKNOWN;
     
     public static MainMenu getValue(int n) {
         // 정수 n을 enum 타입 값으로 변환해서 리턴.
