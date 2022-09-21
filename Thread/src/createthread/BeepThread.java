7c1b5d1c5ce39963661324554e3cf9f3bc098312
ByeonJuHwan <asdcz@naver.com>
2022-09-21 16:45:36 +0900

    스레드를 하위 클래스로 작성하는 2가지 방법

---
+++ Thread/src/createthread/BeepThread.java
@@ -0,0 +1,16 @@
+package createthread;
+
+import java.awt.Toolkit;
+
+public class BeepThread extends Thread{
+    @Override
+    public void run() {
+     // 비프음을 5번 반복해서 소리나게 하는 작업
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i=0; i<5; i++) {
+            toolkit.beep();
+            try {Thread.sleep(500);}catch(Exception e) {}
+                // 1000 을 주게 되면 1초가되고, 500을 주면 0.5초가 된다.
+        }
+    }
+}
