bf70184bb7ea623c4cc49015311c96a89198143b
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2022-09-28 22:03:45 +0900

    데몬스레드

---
+++ Thread/src/daemon/DaemonExample.java
@@ -0,0 +1,18 @@
+package daemon;
+
+public class DaemonExample {
+
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {}
+		
+		System.out.println("메인 스레드 종료");
+	}
+
+}
